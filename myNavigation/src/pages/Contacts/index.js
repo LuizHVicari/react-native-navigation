ea0994b1d26e7af1dc3dbfc311a8ea3d49bb4ae1
LuizHVicari <luizhenrique.vicari@gmail.com>
2023-08-16 15:21:02 -0300

    stack navigator

---
+++ myNavigation/src/pages/Contacts/index.js
@@ -0,0 +1,12 @@
+import { View, Text } from "react-native";
+
+export default function Contacts({navigation}){
+    return(
+        <View>
+            <Text>João</Text>
+            <Text
+                onPress={() => navigation.navigate('Information')}
+            >Information...</Text>
+        </View>
+    )
+}
